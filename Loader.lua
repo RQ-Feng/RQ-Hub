73e52461a3b6a8085081f6854218ed755dca8ecb
RQ_Feng <mrfeng1234561112022@gmail.com>
2025-10-20 22:20:27 +0800

    update Loader

--- Loader.lua
+++ Loader.lua
@@ -1,17 +1,18 @@
 print("--------------------成功注入，正在加载中--------------------")
 local function load(url) return loadstring(game:HttpGet(url))() end
 local OrionLib = load('https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua')
-local baseUrl = "https://raw.githubusercontent.com/RQ-Feng/RQ-Hub/"
+local baseUrl = "https://raw.githubusercontent.com/RQ-Feng/RQ-Hub/refs/heads/main/"
 local PlaceTable = load(baseUrl .. "PlaceTable.lua")
 
-local Game,Place
-Game = PlaceTable[game.GameId]
-if Game then Place = Game.PlaceId[game.PlaceId] end
+local GameId,PlaceId
+GameId = PlaceTable[game.GameId]
+if GameId then PlaceId = GameId.PlaceId[game.PlaceId] end
 
-if not Game or not Place then OrionLib:MakeNotification({Name = "不支持此地点",Content = "请使用其他中心",Time = 5}) return end
+if not GameId or not PlaceId then OrionLib:MakeNotification({Name = "不支持此地点",Content = "请使用其他中心",Time = 5}) return end
 OrionLib:MakeNotification({
-    Name = "加载中...",
-    Content = "可能会有短暂卡顿",
+    Name = "加载中",
+    Content = "请稍等...",
     Time = 5
 })
-load(baseUrl .. "Place-Scripts/" .. Game.Folder .. '/' .. Place .. ".lua") -- 加载链接
+load('https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/Other-script/Setting.lua')-- UI设置
+load(baseUrl .. "Place-Scripts/" .. GameId.Folder .. '/' .. PlaceId .. ".lua") -- 加载链接 
