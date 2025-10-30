6f4ba843ec353e77304df67a853867c6a3192a38
RQ_Feng <mrfeng1234561112022@gmail.com>
2025-10-30 09:18:32 +0800

    update

--- Other-Scripts/Evade currency collecter.lua
+++ Other-Scripts/Evade currency collecter.lua
@@ -1,3 +1,4 @@
+--临时
 OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua'))()
 local ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/mstudio45/MSESP/refs/heads/main/source.luau"))()
 ESPLibrary.GlobalConfig.Rainbow = true
@@ -6,10 +7,18 @@ OrionLib:MakeNotification({
     Content = "请稍等...",
     Time = 5
 })
+
 loadstring(game:HttpGet('https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/Other-script/Setting.lua'))()
 
+local Players = game:GetService('Players')
+local LocalPlayer = Players.LocalPlayer
+local char = LocalPlayer.Character
+LocalPlayer.CharacterAdded:Connect(function(newchar) char = newchar end)
+
+local VisualEsp,AutoCollectCurrency,AutoZoom,AvoidEntityByAnchor
 local Tickets = workspace.Game.Effects.Tickets
-local VisualEsp
+local Zoom = LocalPlayer.PlayerScripts.Camera.FOVAdjusters.Zoom
+
 local function Notify(name,content,Sound,SoundId) -- 信息
     OrionLib:MakeNotification({
         Name = name,
@@ -21,13 +30,7 @@ local function Notify(name,content,Sound,SoundId) -- 信息
     })
 end
 
-local function CollectCurrency(str)
-    game:GetService("ReplicatedStorage").Events.Collectibles.Invoke:InvokeServer(
-        game:GetService("Players").LocalPlayer,str,"Collect"
-    )
-end
-
-local function esp(inst,v)
+local function esp(inst,value)
     local ESPElement = ESPLibrary:Add({
         Name = inst.Name,
         Model = inst,
@@ -35,14 +38,41 @@ local function esp(inst,v)
         TextSize = 17,
         ESPType = "Highlight"
     })
-    repeat task.wait() until not v
+    repeat task.wait() until not value
     ESPElement:Destroy()
 end
 
-local function AddConnection(signal,func)
-    local event = signal:Connect(func)
-    repeat task.wait() until not OrionLib:IsRunning()
-    event:Disconnect()
+local function AddConnection(signal,func,value)
+    pcall(function() 
+        value = value or true
+        local event = signal:Connect(func)
+        repeat task.wait() until not OrionLib:IsRunning() or not value
+        event:Disconnect()
+    end)
+end
+
+local function AvoidEntityAnchorFunction()
+    task.spawn(function() 
+        while AvoidEntityByAnchor do
+            char:WaitForChild('HumanoidRootPart').Anchored = true
+            char:PivotTo(CFrame.new(0,10000,0))
+            wait(10)
+        end
+    end)
+end
+
+local function CollectBread(bread)
+    if not char then return end
+    local AutoCollectCurrencBackup;AutoCollectCurrencyBackup = not AutoCollectCurrency
+    if AutoCollectCurrency then 
+        AutoCollectCurrency = false 
+        char:WaitForChild('HumanoidRootPart').Anchored = false
+    end
+    repeat char:PivotTo(bread:WaitForChild('HumanoidRootPart').CFrame) wait() until not bread
+    if AutoCollectCurrencyBackup then 
+        AvoidEntityByAnchor = true
+        AvoidEntityAnchorFunction() 
+    end
 end
 
 Window = OrionLib:MakeWindow({
@@ -59,45 +89,36 @@ Esp = Window:MakeTab({
     Name = "透视",
     Icon = "rbxassetid://4483345998"
 })
-Esp:AddToggle({
-    Name = "自动收集活动货币",
-    Default = true,
-    Flag = "AutoCollectCurrency",
-    Callback = function()
-        -- local collect = require(game:GetService("ReplicatedStorage").Modules.Client.Loader.CollectablesController.CollectableClientContext.CollectableClient)
-        -- local signal = require(game:GetService("ReplicatedStorage").Modules.Generics.Signal)
-
-        -- local oldfunc = hookfunction(collect.new,function(cc, id, cd)
-        --     v8 = {
-        --         ["CollectableData"] = cd,
-        --         ["ID"] = id,
-        --         ["CollectableContext"] = cc,
-        --         ["Properties"] = {},
-        --         ["EnforcedProperties"] = {},
-        --         ["PropertyAdded"] = signal.new(),
-        --         ["PropertyRemoved"] = signal.new()
-        --     }
-        --     task.spawn(function() 
-        --         while v8.ID and (not getfenv().StopHook) do
-        --             game:GetService("ReplicatedStorage").Events.Collectibles.Invoke:InvokeServer(game:GetService("Players").LocalPlayer,v8.ID,"Collect")
-        --         end
-        --     end)
-        --     local v9 = collect
-        --     return setmetatable(v8, v9)
-        -- end)
-        -- repeat wait() until getfenv().StopHook
-        -- getfenv().StopHook = nil
-        -- hookfunction(collect.new,oldfunc)
-    end
-})
 Tab:AddSlider({
     Name = "缩放",
     Min = 0,
     Max = 1.4,
     Default = 1,
     Increment = 0.1,
-    Callback = function(Value)
-        game:GetService("Players").LocalPlayer.PlayerScripts.Camera.FOVAdjusters.Zoom.Value = Value
+    Flag = 'AutoZoom',
+    Callback = function(value)
+        Zoom.Value = value
+    end
+})
+Tab:AddToggle({
+    Name = "自动收集活动货币",
+    Default = false,
+    Callback = function(value)
+        AutoCollectCurrency = value
+        if AutoCollectCurrency then
+            for _,bread in pairs(Tickets:GetChildren()) do CollectBread(bread) end
+            AddConnection(Tickets.ChildAdded,function(bread) 
+                if AutoCollectCurrency then CollectBread(bread) end
+            end,AutoCollectCurrency)
+        end
+    end
+})
+Tab:AddToggle({
+    Name = "躲避实体(固定传送)",
+    Default = false,
+    Callback = function(value)
+        AvoidEntityByAnchor = value
+        if AvoidEntityByAnchor and char then AvoidEntityAnchorFunction() end
     end
 })
 Esp:AddToggle({
@@ -107,9 +128,10 @@ Esp:AddToggle({
     Callback = function(value)
         VisualEsp = value
         if VisualEsp then
-            for _,visual in pairs(Tickets:GetChildren()) do
-                if visual.Name ~= 'Visual' then return end
-                esp(visual,VisualEsp)
+            for _,visual in pairs(Tickets:GetChildren()) do 
+                if visual.Name == 'Visual' then 
+                    esp(visual,VisualEsp) 
+                end 
             end
         end
     end
@@ -119,6 +141,8 @@ AddConnection(Tickets.ChildAdded,function(inst) -- 其他
     if inst.Name ~= 'Visual' then return end
     if VisualEsp then esp(inst,VisualEsp) end
 end)
+
+AddConnection(Zoom.Changed,function() Zoom.Value = OrionLib.Flags['AutoZoom'].Value end)
 
 task.spawn(function()
     repeat task.wait() until not OrionLib:IsRunning()
