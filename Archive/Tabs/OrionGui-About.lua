ef7ed95444affce8441b3871bd7574c7bb6735f4
RQ_Feng <mrfeng1234561112022@gmail.com>
2025-12-02 10:55:19 +0800

    移除存档文件

--- Archive/Tabs/OrionGui-About.lua
+++
@@ -1,8 +0,0 @@
-others = Window:MakeTab({
-    Name = "其他",
-    Icon = "rbxassetid://4483345998"
-})
-others:AddLabel("此服务器上的游戏ID为:" .. game.GameId)
-others:AddLabel("此服务器位置ID为:" .. game.PlaceId)
-others:AddLabel("此服务器上的游戏版本为:version_" .. game.PlaceVersion)
-others:AddParagraph("此服务器UUID为:", game.JobId)
