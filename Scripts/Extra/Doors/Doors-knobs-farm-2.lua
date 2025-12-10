f0eb81a61cc8efb4feba5021423d6baea919410b
RQ_Feng <RQ_Feng@outlook.com>
2025-12-10 14:36:25 +0800

    移除测试代码

--- Scripts/Extra/Doors/Doors-knobs-farm-2.lua
+++ Scripts/Extra/Doors/Doors-knobs-farm-2.lua
@@ -1,17 +1,3 @@
-function TableVisualization(Table)
-	if type(Table) ~= 'table' then return end
-	warn('table可视化输出:')
-	local function forTable(ForTable,arg)
-		arg = arg or ''
-		for i,v in pairs(ForTable) do
-			if type(v) == 'table' then print(arg..'[\''..tostring(i)..'\']') forTable(v,arg..'∣')
-			else print(arg..'[\''..tostring(i)..'\'] -> '..tostring(v)) end
-		end
-	end
-	forTable(Table)
-end
-
-
 local function Notity(Text)
     local NotifyTable = {
         Title = 'Doors knobs farm',
