be86bb985d3a64961adc2aafc6b3bb62fa37daa8
RQ_Feng <mrfeng1234561112022@gmail.com>
2026-01-11 21:43:48 +0800

    优化 SendNotification 使用逻辑

--- Loader.lua
+++ Loader.lua
@@ -2,30 +2,31 @@ print("--------------------成功注入，正在加载中--------------------")
 local baseUrl = "https://raw.githubusercontent.com/RQ-Feng/RQ-Hub/refs/heads/main/Scripts/"
 local PlaceTable = loadstring(game:HttpGet(baseUrl .. "PlaceTable.lua"))()
 
+local StarterGui = game:GetService("StarterGui")
+local function VanillaNotify(text,duration,icon)
+    StarterGui:SetCore("SendNotification",{
+        Title = "RQ-Hub",
+        Text = text or '',
+        Duration = duration or 5,
+        Icon = icon
+    })
+end
+
 local GameId,PlaceId = game.GameId,game.PlaceId
 
 local Game = PlaceTable[GameId]
 local Place = Game and Game.PlaceId[PlaceId]
 --检查Place
-if not Game or not Place then game:GetService("StarterGui"):SetCore("SendNotification",{Title = "不支持此地点",Text = "请使用其他中心",Duration = 5}); return end
+if not Game or not Place then VanillaNotify(); return end
 
 OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua'))()
 ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/mstudio45/MSESP/refs/heads/main/source.luau"))()
 RQHub = loadstring(game:HttpGet(baseUrl .. 'baseHub_table.lua'))()
 ExecutorChecker = loadstring(game:HttpGet(baseUrl .. 'Utills/ExecutorChecker.lua'))()--检测函数
 
-local checklist = {OrionLib,ESPLibrary,RQHub,ExecutorChecker}; if not checklist[4] then 
-    game:GetService("StarterGui"):SetCore("SendNotification",{
-        Title = "RQHub",Text = "加载资源时遇到问题.",
-        Duration = 10
-    });return
-end
-
-OrionLib:MakeNotification({
-    Name = "加载中",
-    Content = "请稍等...",
-    Time = 5
-})
+local checklist = {OrionLib,ESPLibrary,RQHub,ExecutorChecker}
+if not checklist[4] then VanillaNotify('加载资源时遇到问题',10,'rbxassetid://7733658271'); return end
+VanillaNotify('正在加载,请稍等...',3,'rbxassetid://7733715400')
 
 Window = OrionLib:MakeWindow({
     IntroText = "RQHub-WIP",
