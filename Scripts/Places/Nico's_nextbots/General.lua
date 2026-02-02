11efc9daffb14ab667b44189ef7886468d26ceda
RQ_Feng <mrfeng1234561112022@gmail.com>
2026-02-02 22:15:03 +0800

    更新 Nico

--- Scripts/Places/Nico's_nextbots/General.lua
+++ Scripts/Places/Nico's_nextbots/General.lua
@@ -3,10 +3,11 @@ Tab = Window:MakeTab({
     Name = "主界面",
     Icon = "rbxassetid://4483345998"
 })
+Tab:AddLabel('当前处于WIP阶段.')
 Tab:AddSection({Name = "Punches"})
 local punches = LocalPlayer.stats.punches
-local LatestRoomLabel = Tab:AddLabel('当前有 '.. punches.Value ..' 个punches.')
-AddConnection(punches.Changed,function() LatestRoomLabel:Set('当前有 '.. punches.Value ..' 个punches.') end)
+local LatestRoomLabel = Tab:AddLabel('当前有 '.. punches.Value ..' 个Punches.')
+AddConnection(punches.Changed,function() LatestRoomLabel:Set('当前有 '.. punches.Value ..' 个Punches.') end)
 Tab:AddToggle({
     Name = "刷Punches",
     Save = true,
@@ -17,4 +18,15 @@ Tab:AddToggle({
             events.player:FindFirstChild('local').punch:FireServer()
         end
     end
+})
+Tab:AddSection({Name = "其他"})
+Tab:AddButton({
+    Name = "紫砂",
+    ClickTwice = true,
+    Callback = function() events.player.char.ClientDeath:FireServer() end
+})
+Tab:AddButton({
+    Name = "重生",
+    ClickTwice = true,
+    Callback = function() events.player.char.respawnchar:FireServer() end
 })
