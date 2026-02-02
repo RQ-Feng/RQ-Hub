48d95214a049d572c984ddd8c96dcf2bea1882cf
RQ_Feng <mrfeng1234561112022@gmail.com>
2026-02-02 21:52:31 +0800

    更新 Loader

--- Loader.lua
+++ Loader.lua
@@ -15,25 +15,38 @@ local PlaceTable = loadstring(game:HttpGet(baseUrl .. "PlaceTable.lua"))()
 
 local GameId,PlaceId = game.GameId,game.PlaceId
 
-local Game = PlaceTable[GameId]
-local Place = Game and Game.PlaceId[PlaceId]
+local gameInfo = PlaceTable[GameId]
+local placeInfo = gameInfo and (gameInfo.PlaceId['*'] or gameInfo.PlaceId[PlaceId])
 --检查Place
-if not Game or not Place then VanillaNotify('不支持此地点.'); return end
+if not gameInfo or not placeInfo then VanillaNotify('不支持此地点.'); return end
 
-OrionLib = loadstring(game:HttpGet('https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua'))()
-ESPLibrary = loadstring(game:HttpGet("https://raw.githubusercontent.com/mstudio45/MSESP/refs/heads/main/source.luau"))()
-RQHub = loadstring(game:HttpGet(baseUrl .. 'baseHub_table.lua'))()
-ExecutorChecker = loadstring(game:HttpGet(baseUrl .. 'Utills/ExecutorChecker.lua'))()--检测函数
+local LoadSuc,_ = pcall(function()
+    local baseScriptURLs = {
+        'https://raw.githubusercontent.com/RQ-Feng/Orion/refs/heads/main/main.lua',
+        'https://raw.githubusercontent.com/mstudio45/MSESP/refs/heads/main/source.luau',
+        baseUrl .. 'baseHub_table.lua',
+        baseUrl .. 'Utills/ExecutorChecker.lua'
+    }
+    local baseScripts = {}
+    
+    for i,url in ipairs(baseScriptURLs) do task.spawn(function() baseScripts[i] = game:HttpGet(url) end) end
+    repeat task.wait() until #baseScripts == 4 --Waiting for scripts
+
+    OrionLib = loadstring(baseScripts[1])()
+    ESPLibrary = loadstring(baseScripts[2])()
+    RQHub = loadstring(baseScripts[3])()
+    ExecutorChecker = loadstring(baseScripts[4])()
+end)
 
 local checklist = {OrionLib,ESPLibrary,RQHub,ExecutorChecker}
-if not checklist[4] then VanillaNotify('加载资源时遇到问题',10,'rbxassetid://7733658271'); return end
+if not LoadSuc or not checklist[4] then VanillaNotify('加载资源时遇到问题',10,'rbxassetid://7733658271'); return end
 
-local GameFolder = 'RQHub\\'..Game.Folder
+local GameFolder = 'RQHub\\' .. gameInfo.Folder
 if not isfolder(GameFolder) then makefolder(GameFolder) end
 
 local ScriptURLs = {
     baseUrl .. 'Utills/Init.lua',
-    baseUrl .. "Places/" .. Game.Folder .. '/' .. Place .. ".lua",
+    baseUrl .. "Places/" .. gameInfo.Folder .. '/' .. (placeInfo ~= '*' and placeInfo or '') .. ".lua",
     baseUrl .. 'Utills/EspSetting.lua',
 }
 local Scripts = {}
@@ -42,14 +55,16 @@ task.spawn(function()
     for _,url in ipairs(ScriptURLs) do table.insert(Scripts,game:HttpGet(url)) end --Load scripts from urls
 end)
 
+local Suffix = gameInfo.Folder .. (placeInfo ~= '*' and (' - ' .. placeInfo) or '')
+
 Window = OrionLib:MakeWindow({
     IntroText = "RQHub-WIP",
-    Name = 'RQHub | '..Game.Folder..' - '..Place,
+    Name = 'RQHub | ' .. Suffix,
     SaveConfig = true,
-    ConfigFolder = GameFolder..'\\'..Place
+    ConfigFolder = GameFolder .. (placeInfo ~= '*' and ('\\' .. placeInfo) or '')
 })
 
-repeat task.wait() until #Scripts == 4 --Waiting for scripts
+repeat task.wait() until #Scripts == 3 --Waiting for scripts
 
 for _,script in pairs(Scripts) do loadstring(script)() end
 
